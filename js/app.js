c267e14da1ddce165322bae5571fc313928e9490
Joshua Pickard <joshuapickard.jp@gmail.com>
2018-06-28 08:34:52 -0400

    added shuffle and flip

--- js/app.js
+++ js/app.js
@@ -6,20 +6,23 @@ const pics = ['fa fa-diamond','fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-pap
 
 const table = document.querySelector('.deck');
  //   - loop through each card and create its HTML
- for (let i = 0; i<pics.length; i++) {
+
+for (let i = 0; i<pics.length; i++) {
  	const cards = document.createElement('li');
  	cards.classList.add('card');
  	cards.innerHTML = '<i class="' + pics[i] +'"></i>';
  	//   - add each card's HTML to the page
- 	table.appendChild(cards); 	
- }
+	table.appendChild(cards); 	
+}
 
 
 /*
  * Display the cards on the page
 
  *   - shuffle the list of cards using the provided "shuffle" method below
+*/
 
+/*
  
  */
 
@@ -37,27 +40,81 @@ function shuffle(array) {
 
     return array;
 }
+let flippedCard = [];
+let matchedCard = [];
+const numMoves = document.getElementsByClassName('moves')
+
+let move = 0
+
 // set up the event listener for a card. If a card is clicked:
 table.addEventListener('click', flipCard, false);
 //  - display the card's symbol (put this functionality in another function that you call from this one)
 function flipCard(evt) {
 	if (evt.target !== evt.currentTarget) {
 		const clickedCard = evt.target;
-		clickedCard.classList.add('show', 'open')			
+		clickedCard.classList.add('show', 'open');
+		flippedCard.push(clickedCard);
+				
+		console.log(move)
+		//if the list already has another card, check to see if the two cards match
+		if (flippedCard > 2){
+			firstCard.classList.remove('show', 'open');
+			secondCard.classList.remove('show', 'open');
+		}
+		const firstCard = flippedCard[0];
+		const secondCard = flippedCard[1];
+		
+		
+		if (flippedCard.length === 2 && firstCard.innerHTML == flippedCard[1].innerHTML && 
+			firstCard.innerHTML === flippedCard[1].innerHTML){
+			//if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+
+			clickedCard.classList.add('match');
+			firstCard.classList.add('match')
+			console.log('match!!');	
+			flippedCard = [];
+			matchedCard.push(firstCard, secondCard);
+			move +=1
+			gameWon();
+
+		}else if (flippedCard.length === 2 && firstCard.innerHTML != flippedCard[1].innerHTML){
+			console.log('no match');
+
+			setTimeout(wait, 1000);
+			function wait(){
+				firstCard.classList.remove('show', 'open');			
+				secondCard.classList.remove('show', 'open')
+				
+			}
+			flippedCard = [];
+			move +=1
+		}else {
+			console.log('select another');
+			move +=1
+		}
 	}
 
 	evt.stopPropagation();
 }
 
+function gameWon(){
+	setTimeout (wait, 100);
+	function wait() {
+		if (pics.length === matchedCard.length) {
+		modal('you won!!');
+		}
 
+	}
+	
+}
 
 
 /*
  
  
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *  - 
+ *    + 
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
